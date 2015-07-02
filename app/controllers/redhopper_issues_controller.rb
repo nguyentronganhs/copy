eebc87bdf97d4b1d2994338ab45d871dfec2d220
David F <david@infopiiaf.fr>
2015-07-02 16:17:29 +0200

    Move up a sorted issue. refs #1632

--- app/controllers/redhopper_issues_controller.rb
+++ app/controllers/redhopper_issues_controller.rb
@@ -6,4 +6,18 @@ class RedhopperIssuesController < ApplicationController
 
     redirect_to project_kanbans_path(redhopper_issue.issue.project)
   end
+
+  def move_up
+    issue_to_move_up = RedhopperIssue.where(issue_id: params[:issue_id]).first
+    issue_to_move_down = RedhopperIssue.where(issue_id: params[:insert_before_id]).first
+
+    if issue_to_move_down.previous
+      issue_to_move_up.append_to issue_to_move_down.previous
+    else
+      issue_to_move_up.prepend
+    end
+
+    redirect_to project_kanbans_path(issue_to_move_up.issue.project)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,4 @@
 get '/projects/:project_id/issues/kanbans', :to => 'kanbans#index', :as => 'project_kanbans'
 
 post '/redhopper_issues', :to => 'redhopper_issues#create', :as => 'create_redhopper_issue'
+post '/redhopper_issues/move_up', :to => 'redhopper_issues#move_up', :as => 'move_up_redhopper_issue'
