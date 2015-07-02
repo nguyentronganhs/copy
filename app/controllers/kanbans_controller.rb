11c67175cc763e5abd3194d2980bd39de8f1dcb7
David F <david@infopiiaf.fr>
2015-07-02 15:11:51 +0200

    Splitted columns: sorted/unsorted. refs #1632

--- app/controllers/kanbans_controller.rb
+++ app/controllers/kanbans_controller.rb
@@ -3,15 +3,27 @@ class KanbansController < ApplicationController
 
   def index
     @issue_statuses = IssueStatus.sorted
-    empty_issues_by_status = @issue_statuses.inject({}) do |result, issue_status|
-      result[issue_status] = []
+    @issues_by_status = @issue_statuses.inject({}) do |result, issue_status|
+      result[issue_status] = {
+        sorted: [],
+        unsorted: []
+      }
       result
     end
 
     @project = Project.find(params[:project_id])
-    @issues_by_status = Issue.visible(User.current, :project => @project).where(tracker: Setting.plugin_redhopper["displayed_tracker_ids"]).inject(empty_issues_by_status) do |result, issue|
-      result[issue.status] << issue
-      result
+    project_issues = Issue.visible(User.current, :project => @project)
+    if Setting.plugin_redhopper
+      project_issues = project_issues.where(tracker: Setting.plugin_redhopper["displayed_tracker_ids"])
+    end
+    sorted_issues = RedhopperIssue.ordered
+
+    sorted_issues.each do |issue|
+      @issues_by_status[issue.issue.status][:sorted] << issue.issue if project_issues.include?(issue.issue)
+    end
+
+    project_issues.each do |issue|
+      @issues_by_status[issue.status][:unsorted] << issue unless @issues_by_status[issue.status][:sorted].include?(issue)
     end
   end
 end
