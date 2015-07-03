a50327d0812c8d8b3d87627bd01877c065f699f6
David F <david@infopiiaf.fr>
2015-07-03 17:33:50 +0200

    Drag and drop to change priorities. refs #1623

--- assets/javascripts/redhopper.js
+++ assets/javascripts/redhopper.js
@@ -1,11 +1,24 @@
 (function ($) {
 	$(function () {
 		var dragSrcEl;
+		var dragSrcList;
+		var dropzones;
+		var dropzoneHeight;
 
 		function handleDragStart(e) {
 			this.style.opacity = '0.6';  // this / e.target is the source node.
 
 			dragSrcEl = this;
+			dragSrcList = $(this).parents('ol').first();
+			dropzones = $(dragSrcList).children('hr');
+
+			dropzones.attr('dropzone', 'move');
+			[].forEach.call(dropzones, function(col) {
+				col.addEventListener('dragenter', handleDragEnter, false)
+				col.addEventListener('dragover', handleDragOver, false);
+				col.addEventListener('dragleave', handleDragLeave, false);
+				col.addEventListener('drop', handleDrop, false);
+			});
 
 			e.dataTransfer.effectAllowed = 'move';
 			e.dataTransfer.setData('text/html', this.innerHTML);
@@ -13,9 +26,20 @@
 
 		function handleDragEnd(e) {
 			this.style.opacity = '1';  // this / e.target is the source node.
+
+			dropzones.attr('dropzone', '');
+			[].forEach.call(dropzones, function(col) {
+				col.removeEventListener('dragenter', handleDragEnter, false)
+				col.removeEventListener('dragover', handleDragOver, false);
+				col.removeEventListener('dragleave', handleDragLeave, false);
+				col.removeEventListener('drop', handleDrop, false);
+			});
 		}
 
 		function handleDragEnter(e) {
+			dropzoneHeight = $(this).height();
+			$(this).height($(dragSrcEl).height());
+
 			this.classList.add('dragover');  // this / e.target is the target node.
 		}
 
@@ -27,21 +51,31 @@
 
 		function handleDragLeave(e) {
 			this.classList.remove('dragover');  // this / e.target is the target node.
+
+			$(this).height(dropzoneHeight);
 		}
 
 		function handleDrop(e) {
 			// this/e.target is current target element.
+			this.classList.remove('dragover');  // this / e.target is the target node.
 
 			if (e.stopPropagation) {
 				e.stopPropagation(); // Stops some browsers from redirecting.
 			}
 
+			var issueToMove = $(dragSrcEl).attr('id').split('-')[1]
+			var targetComponents = $(this).attr('id').split('-')
+			var targetIssue = targetComponents[1]
+			var movePosition = targetComponents[2]
 			// Don't do anything if dropping the same column we're dragging.
-			if (dragSrcEl != this) {
-				// Set the source column's HTML to the HTML of the column we dropped on.
-				dragSrcEl.innerHTML = this.innerHTML;
-				this.innerHTML = e.dataTransfer.getData('text/html');
-				this.classList.remove('dragover');  // this / e.target is the target node.
+			if (issueToMove !== targetIssue) {
+				$.post('/redhopper_issues/move', {
+					issue_id: issueToMove,
+					target_issue_id: targetIssue,
+					insert: movePosition
+				}, function () {
+					window.location.href = window.location.href;
+				})
 			}
 
 			return false;
@@ -51,14 +85,6 @@
 		[].forEach.call(issues, function(col) {
 			col.addEventListener('dragstart', handleDragStart, false);
 			col.addEventListener('dragend', handleDragEnd, false);
-		});
-
-		issues = document.querySelectorAll('ol.kanban [dropzone="move"]');
-		[].forEach.call(issues, function(col) {
-			col.addEventListener('dragenter', handleDragEnter, false)
-			col.addEventListener('dragover', handleDragOver, false);
-			col.addEventListener('dragleave', handleDragLeave, false);
-			col.addEventListener('drop', handleDrop, false);
 		});
 	})
 })(jQuery)
