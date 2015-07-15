b0e89d016656df1e6418d7703d464abd6bfbded7
David F <david@infopiiaf.fr>
2015-07-15 14:43:59 +0200

    Caracterisation test before refactor. refs #1665

--- test/functional/kanbans_controller_test.rb
+++ test/functional/kanbans_controller_test.rb
@@ -1,4 +1,20 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class KanbansControllerTest < ActionController::TestCase
+
+	def setup
+		@todo = IssueStatus.create! name: 'To Do'
+		@doing = IssueStatus.create! name: 'Doing'
+		@done = IssueStatus.create! name: 'Done'
+
+		@project = Project.create! name: 'Test Project', identifier: 'test-project'
+	end
+
+	def test_index
+		get :index, :project_id => @project.id
+
+		assert_response :success
+		assert_template 'index'
+		assert_not_nil assigns['issues_by_status']
+	end
 end
