fe2bc70676c7e14e38d992f799311dcba6ef6b7b
David F <david.friquet@infopiiaf.fr>
2015-07-17 16:53:27 +0200

    Displaying a hint on blocked issues. refs #1620

--- app/controllers/kanbans_controller.rb
+++ app/controllers/kanbans_controller.rb
@@ -5,5 +5,7 @@ class KanbansController < ApplicationController
     @project = Project.find(params[:project_id])
 
     @kanban_board = KanbanBoard.new @project
+
+    @feature_show_blockers = Feature.enabled("show_blockers")
   end
 end
--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redhopper do
   name 'Redhopper plugin'
   author 'infoPiiaf'
   description 'Kanban boards for Redmine, inspired by Jira Agile (formerly known as Greenhopper), but following its own path.'
-  version '0.7.0'
+  version '0.8.0.dev'
   url 'http://www.infopiiaf.fr'
   author_url 'http://www.infopiiaf.fr'
 
