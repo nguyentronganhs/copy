a536d7bf375764207b77a0f35825e6aff4ed84d5
David F <david.friquet@infopiiaf.fr>
2015-07-17 15:30:39 +0200

    Bumping version. refs #1666

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redhopper do
   name 'Redhopper plugin'
   author 'infoPiiaf'
   description 'Kanban boards for Redmine, inspired by Jira Agile (formerly known as Greenhopper), but following its own path.'
-  version '0.6.0'
+  version '0.7.0'
   url 'http://www.infopiiaf.fr'
   author_url 'http://www.infopiiaf.fr'
 
