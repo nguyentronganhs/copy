f5e0097aac3166b98ecdb0293707ab11adc16239
David F <david.friquet@infopiiaf.fr>
2015-07-20 14:19:54 +0200

    Adding remaining days before due date. fixes #1635

--- app/controllers/kanbans_controller.rb
+++ app/controllers/kanbans_controller.rb
@@ -7,5 +7,6 @@ class KanbansController < ApplicationController
     @kanban_board = KanbanBoard.new @project
 
     @feature_show_blockers = Feature.enabled("show_blockers")
+    @feature_show_due_date = Feature.enabled("show_due_date")
   end
 end
--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redhopper do
   name 'Redhopper plugin'
   author 'infoPiiaf'
   description 'Kanban boards for Redmine, inspired by Jira Agile (formerly known as Greenhopper), but following its own path.'
-  version '0.8.1'
+  version '0.9.0'
   url 'https://gitlab.com/infoPiiaf/redhopper.git'
   author_url 'http://www.infopiiaf.fr'
 
