2489d1bf25bf34f41704e9f53ce416166e1b4b02
Alexandre Friquet <alexandre.friquet@infopiiaf.fr>
2015-09-23 17:16:03 +0200

    Removes switch for due date info: refs #1749.

--- app/controllers/kanbans_controller.rb
+++ app/controllers/kanbans_controller.rb
@@ -6,7 +6,6 @@ class KanbansController < ApplicationController
 
     @kanban_board = KanbanBoard.new @project
 
-    @feature_show_due_date = Feature.enabled("show_due_date")
     @feature_block_with_comment = Feature.enabled("block_with_comment")
   end
 end
