14c4f3a55110502311929a19af58c7af195cea97
Alexandre Friquet <alexandre.friquet@infopiiaf.fr>
2015-09-23 17:28:23 +0200

    Removes switch for blocked by a comment: refs #1750.

--- app/controllers/kanbans_controller.rb
+++ app/controllers/kanbans_controller.rb
@@ -5,7 +5,6 @@ class KanbansController < ApplicationController
     @project = Project.find(params[:project_id])
 
     @kanban_board = KanbanBoard.new @project
-
-    @feature_block_with_comment = Feature.enabled("block_with_comment")
   end
+
 end
