03f678ad0ec0a9683dc818804ebbe00b8f0d847e
David F <david.friquet@infopiiaf.fr>
2015-10-13 13:36:52 +0200

    Testing Redhopper move action. refs #1762

--- test/functional/redhopper_issues_controller_test.rb
+++ test/functional/redhopper_issues_controller_test.rb
@@ -14,11 +14,50 @@ class RedhopperIssuesControllerTest < ActionController::TestCase
            :trackers,
            :projects_trackers
 
-	def test_create
-    expected_issue_id = 1
-		post :create, :issue_id => expected_issue_id
+  def setup
+    @kanban = RedhopperIssue.create! issue: Issue.find(1)
+  end
 
-		assert_redirected_to project_kanbans_path(Issue.find(expected_issue_id).project)
-	end
+  def test_create
+    # Given
+    requested_issue = Issue.find(2)
+    # When
+    assert_difference('RedhopperIssue.count', +1) do
+      post :create, :issue_id => requested_issue.id
+    end
+    # Then
+    assert_redirected_to project_kanbans_path(requested_issue.project)
+  end
+
+  def test_move_in_first_place
+    # Given
+    to_move_up = RedhopperIssue.create! issue: Issue.find(2)
+    # When
+    get :move, id: to_move_up.id, target_id: @kanban.id, insert: 'before'
+    # Then
+    assert_equal [to_move_up, @kanban], RedhopperIssue.ordered
+    assert_redirected_to project_kanbans_path(to_move_up.issue.project)
+  end
+
+  def test_move_up_in_second_place
+    # Given
+    second_kanban = RedhopperIssue.create! issue: Issue.find(2)
+    to_move_up = RedhopperIssue.create! issue: Issue.find(3)
+    # When
+    get :move, id: to_move_up.id, target_id: second_kanban.id, insert: 'before'
+    # Then
+    assert_equal [@kanban, to_move_up, second_kanban], RedhopperIssue.ordered
+    assert_redirected_to project_kanbans_path(to_move_up.issue.project)
+  end
+
+  def test_move_down
+    # Given
+    to_move_up = RedhopperIssue.create! issue: Issue.find(2)
+    # When
+    get :move, id: @kanban.id, target_id: to_move_up.id, insert: 'after'
+    # Then
+    assert_equal [to_move_up, @kanban], RedhopperIssue.ordered
+    assert_redirected_to project_kanbans_path(to_move_up.issue.project)
+  end
 
 end
