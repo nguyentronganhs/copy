b24500774a30b59f6299b9b801282c2e59d795a5
Alexandre Friquet <alexandre.friquet@infopiiaf.fr>
2016-04-28 12:04:02 +0200

    1.0.4 begins.

--- init.rb
+++ init.rb
@@ -22,7 +22,7 @@ Redmine::Plugin.register :redhopper do
   name 'Redhopper plugin'
   author 'infoPiiaf'
   description 'Kanban boards for Redmine, inspired by Jira Agile (formerly known as Greenhopper), but following its own path.'
-  version '1.0.3'
+  version '1.0.4.dev'
   url 'https://git.framasoft.org/infopiiaf/redhopper.git'
   author_url 'http://www.infopiiaf.fr'
 
